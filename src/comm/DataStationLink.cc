3d27f833873063c99a6358b53d059ac1f624c42b
Annerion <krd311@nyu.edu>
2018-04-02 12:04:27 -0500

    Added the skeleton file src/comm/DataStationLink.cc that lays out the basics of how camera traps and QGC will communicate, no code yet.

---
+++ src/comm/DataStationLink.cc
@@ -0,0 +1,13 @@
+//This file will stand control Xbee communication with camera traps like linkManager controls MavLinks with the vehicle. 
+//Skeleton of File:
+
+//Parse in
+//Parse Out
+//Reset DataStation
+//Data Station ID Status Request
+//Save Data Station to List (spreadsheet)
+//Recover Data Station from list by ID
+
+//Data Station List will be stored in parallel with mission logs, /Home/Documents/QGroundControl/DataStations
+
+//Additionally, the gui will need modificaiton to acess these features.
