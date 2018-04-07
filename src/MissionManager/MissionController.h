e5b0eecfa9af78c2cde19a221f1b8ca34d78b33f
Mounir Elgharabawy <mke255@nyu.edu>
2018-04-07 13:40:30 -0400

    fixed a mistake

--- src/MissionManager/MissionController.h
+++ src/MissionManager/MissionController.h
@@ -94,7 +94,7 @@ public:
     /// Add a new simple mission item to the list
     ///     @param i: index to insert at
     /// @return Sequence number for new item
-    Q_INVOKABLE int insertSimpleMissionItem(QGeoCoordinate coordinate, int i, bool cameraTrap = false);
+    Q_INVOKABLE int insertSimpleMissionItem(QGeoCoordinate coordinate, int i);
 
     /// Add a new ROI mission item to the list
     ///     @param i: index to insert at
