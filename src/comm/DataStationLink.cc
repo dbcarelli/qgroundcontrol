d8dd68d2bd3520c607e530961b66638e99995f9b
dbcarelli <dbcarelli@gmail.com>
2018-04-13 20:20:54 +0400

    Read add DataStation files.

---
+++ src/comm/DataStationLink.cc
@@ -0,0 +1,30 @@
+#include "DataStationLink.h"
+
+
+DataStationLink::DataStationLink(QString portname){
+    serialPort = new QSerialPort(portname);
+
+    serialPort->open(QIODevice::ReadWrite);
+    serialPort->setBaudRate(QSerialPort::Baud57600);
+}
+
+int DataStationLink::_write(QString buffer){
+    return serialPort->write(buffer.toUtf8());
+}
+
+QString DataStationLink::_read(size_t size){
+    return QString::fromUtf8(serialPort->read(size));
+
+}
+
+int DataStationLink::setDataStationId(QString newId){
+    QString message = "changin data station id...";
+    _write(message);
+    return 0;
+}
+
+int DataStationLink::deployDataStation(QString targetId){
+    QString message = "data station deployed";
+    _write(message);
+    return 0;
+}
