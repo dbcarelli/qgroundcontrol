b4d8e37a601b3a0483cc61ae00ae5c875069df0d
dbcarelli <dbcarelli@gmail.com>
2018-04-17 02:29:01 -0700

    Add mounir's QGroundControlQmlGlobal

--- src/QmlControls/QGroundControlQmlGlobal.h
+++ src/QmlControls/QGroundControlQmlGlobal.h
@@ -26,6 +26,7 @@
 #include "GPS/GPSManager.h"
 #endif /* __mobile__ */
 #include "GPSRTKFactGroup.h"
+#include "DataStationManager.h"
 
 #ifdef QT_DEBUG
 #include "MockLink.h"
@@ -143,6 +144,7 @@ public:
     QGCCorePlugin*          corePlugin          ()  { return _corePlugin; }
     SettingsManager*        settingsManager     ()  { return _settingsManager; }
     FactGroup*              gpsRtkFactGroup     ()  { return &_gpsRtkFactGroup; }
+    DataStationManager*     dataStationManager  ()  { return _dataStationManager; }
     static QGeoCoordinate   flightMapPosition   ()  { return _coord; }
     static double           flightMapZoom       ()  { return _zoom; }
 
@@ -203,6 +205,7 @@ private:
     FirmwarePluginManager*  _firmwarePluginManager;
     SettingsManager*        _settingsManager;
     GPSRTKFactGroup         _gpsRtkFactGroup;
+    DataStationManager *    _dataStationManager;
 
     bool                    _skipSetupPage;
 
