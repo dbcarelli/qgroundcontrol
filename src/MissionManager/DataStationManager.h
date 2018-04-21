7cb6843d0f159da814c24c6aff3477df5244193e
dbcarelli <dbcarelli@gmail.com>
2018-04-21 06:00:03 -0700

    Resolve rebase conflicts

--- src/MissionManager/DataStationManager.h
+++ src/MissionManager/DataStationManager.h
@@ -24,7 +24,8 @@ private:
     QList<DataStation *> dataStations;
 
 public:
-
+    Q_PROPERTY(QList<DataStation *> dataStations READ getDataStations NOTIFY dataStationsChanged)//add a write when we know what writing will look like
+    QList<DataStation *> getDataStations                  () { return dataStations; }
     DataStationManager(QGCApplication *app, QGCToolbox *toolbox);
     // close _dsLink
     ~DataStationManager();
